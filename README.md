c033ed75987ac1679ed8a80b38d71e365bc9c09f
anhkim <akhoang88@gmail.com>
2014-07-01 16:54:37 -0700

    directory restructured

--- README.md
+++
@@ -1,6 +0,0 @@
-## DevBootcamp
-===========
-
-### a repo for all Dev Bootcamp exercises
-
-1. dbc_overflow - a cms, stackoverflow clone
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DbcOverflow::Application.config.secret_token = '2e265633494e6aa738462f21bd6ffe83f54b59eae35ee57ad9c41c55c8afa4587d323fcbb6699c453fd96be578e976892061bbc0697baac055544119b456ccaf'
