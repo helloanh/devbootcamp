665f2d6e1b713a2eaad6cf7714d9a90f600eeb1c
anhkim <akhoang88@gmail.com>
2014-07-10 22:17:00 -0700

    RoR tutorial ch3

---
+++ ruby_apps_porfolio/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3294ab03dfa230e9389bdb0138e872bb291a8c0fbbbf9918bb1469683d4fa3023ec60102626980ccb5e71043fb7a14fc589919780d6278def4207661667311ff'
