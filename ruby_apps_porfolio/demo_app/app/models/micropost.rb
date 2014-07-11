af0c5f8dc72d6f0d734b275a2c5579d89a1a5a12
anhkim <akhoang88@gmail.com>
2014-07-10 20:42:00 -0700

    finished demo app

--- ruby_apps_porfolio/demo_app/app/models/micropost.rb
+++ ruby_apps_porfolio/demo_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  belongs_to :user
   validates :content, :length => {:maximum => 140}
 end
--- ruby_apps_porfolio/demo_app/app/models/user.rb
+++ ruby_apps_porfolio/demo_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  has_many :microposts
 end
 
