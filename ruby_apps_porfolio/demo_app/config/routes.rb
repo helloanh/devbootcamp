834ef63939cfb75c12266a2f7c835edcbd80063b
anhkim <akhoang88@gmail.com>
2014-07-18 06:06:42 -0700

    add static pages test

--- ruby_apps_porfolio/demo_app/config/routes.rb
+++ ruby_apps_porfolio/demo_app/config/routes.rb
@@ -1,4 +1,8 @@
 DemoApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   resources :microposts
 
   resources :users
---
+++ ruby_apps_porfolio/demo_app/test/integration/static_pages_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StaticPagesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
